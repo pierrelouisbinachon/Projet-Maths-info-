c1c839ffde9ed112cd3af8c15236968b39bc4289
Jean-Edouard Dupau <jean-edouard.dupau@mines-paristech.fr>
2020-10-31 13:21:00 +0100

    test graphique de la fonction Newton

--- Newton.py
+++ Newton.py
@@ -2,6 +2,10 @@ N = 100
 eps = 10**(-10) #totalement choisi au pif
 import autograd
 import autograd.numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = [10, 10] # [width, height] (inches). 
 
 def J(f):
     j = autograd.jacobian
@@ -37,3 +41,34 @@ def f1(x1, x2):
 
 def f2(x1,x2):
     return(np.array([f1(x1,x2) - 0.8, x1 - x2]))
+def f3(x1,x2):
+    return(np.array([f1(x1,x2) - 0.8, x1-x2 -0.3]))
+
+def display_contour(f, x, y, levels):
+    X, Y = np.meshgrid(x, y)
+    Z = f(X, Y)
+    fig, ax = plt.subplots()
+    contour_set = plt.contour(
+        X, Y, Z, colors="grey", linestyles="dashed", 
+        levels=levels 
+    )
+    ax.clabel(contour_set)
+    plt.grid(True)
+    plt.xlabel("$x_1$") 
+    plt.ylabel("$x_2$")
+    plt.gca().set_aspect("equal")
+
+display_contour(
+    f1, 
+    x=np.linspace(-1.0, 1.0, 100), 
+    y=np.linspace(-1.0, 1.0, 100), 
+    levels=10 # 10 levels, automatically selected
+)
+x,y = Newton(f2, 0.8, 0.8)
+
+plt.scatter(np.array([x]), np.array([y]))
+x,y = Newton(f3, np.random.random(), np.random.random())
+plt.scatter(np.array([x]), np.array([y]), color = 'green')
+
+    
+plt.show()
